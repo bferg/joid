6ef7cfe06b495186810d2af54196264268dd74c5
treeder <treeder@aa8f4b0c-f62a-0410-b554-e13d1092c3cc>
2007-07-24 04:50:30 +0000

    Fixed up some build problems after refactoring.

--- trunk/src/examples/scripts/associate.sh
+++ trunk/src/examples/scripts/associate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -classpath ../../../out/joid.jar:../../../out/joid_examples.jar:../../../resources/log4j-1.2.13.jar:../../../resources/tsik.jar examples.consumer.Associate $1
+java -classpath ../../../out/joid.jar:../../../out/joid_examples.jar:../../../lib/log4j-1.2.13.jar:../../../lib/tsik.jar examples.consumer.Associate $1

04df944263fc858791d1e208e405bc35cac9e1a8
akarasulu@gmail.com <akarasulu@gmail.com@aa8f4b0c-f62a-0410-b554-e13d1092c3cc>
2011-03-30 19:32:16 +0000

    Applying patch found here: http://code.google.com/p/joid/issues/detail?id=33&can=6&q=akarasulu

--- lib/README.txt
+++
@@ -1,2 +0,0 @@
-For the latest dependency information, see:
-http://code.google.com/p/joid/wiki/ServerInstallation
--- src/examples/scripts/authenticate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -classpath ../../../out/joid.jar:../../../out/joid_examples.jar:../../../lib/log4j-1.2.13.jar:../../../lib/tsik.jar examples.consumer.Authenticate $1

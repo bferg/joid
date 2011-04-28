da22f97c7215d714fa972013d2e42464db0a93df
birkan.duman@gmail.com <birkan.duman@gmail.com@aa8f4b0c-f62a-0410-b554-e13d1092c3cc>
2011-04-28 18:46:15 +0000

    some minor tweaks in test that starts the relying-party app in embedded jetty

--- trunk/examples/relying-party/src/test/java/org/verisign/joid/examples/server/StartRelyingParty.java
+++ trunk/examples/relying-party/src/test/java/org/verisign/joid/examples/server/StartRelyingParty.java
@@ -12,8 +12,8 @@ public class StartRelyingParty {
 		SocketConnector connector = new SocketConnector();
 		
 		// Set some timeout options to make debugging easier.
-		connector.setMaxIdleTime(1000 * 60 * 60);
-		connector.setSoLingerTime(-1);
+//		connector.setMaxIdleTime(1000 * 60 * 60);
+//		connector.setSoLingerTime(-1);
 		connector.setPort(8080);
 		server.setConnectors(new Connector[] { connector });
 
